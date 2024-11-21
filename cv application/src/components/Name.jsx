0cafc401ba345c000349db1fb79811aec37228a7
Kummirai <ajaxmilton@hotmail.com>
2024-11-21 18:11:58 +0200

    Fix textarea to min-height 8rem

--- cv application/src/components/Name.jsx
+++ cv application/src/components/Name.jsx
@@ -54,11 +54,44 @@ const Name = () => {
       <Textarea
         id="about"
         name="about"
-        row="10"
+        row="20"
         col="70"
         placeholder="Add work experience , press add button and add another work experience and press add button"
       />
       <button>Add</button>
+      <h2 className="name-jobtitle">Skills</h2>
+      <Textarea
+        id="about"
+        name="about"
+        row="10"
+        col="70"
+        placeholder="Add a Skill , press add button and add another skill and press add button"
+      />
+      <button>Add</button>
+      <h2 className="name-jobtitle">Education</h2>
+      <div className="name-title">
+        <div>
+          <p>From</p>
+          <Input
+            type="date"
+            id="year"
+            name="year"
+            placeholder=" Period 2010 - 2012"
+          />
+        </div>
+        <div>
+          <p>To</p>
+          <Input type="date" id="to" name="to" placeholder="2012" />
+        </div>
+      </div>
+      <div className="position name-title">
+        <Input
+          type="text"
+          id="position"
+          name="position"
+          placeholder="Positon / Job Title"
+        />
+      </div>
     </form>
   );
 };
