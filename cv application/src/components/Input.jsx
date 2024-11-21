dc68207e4951f54d422ba6a7b0415cf3aa89ce32
Kummirai <ajaxmilton@hotmail.com>
2024-11-21 16:18:26 +0200

    Create label compont and add it to input elements

--- cv application/src/components/Input.jsx
+++ cv application/src/components/Input.jsx
@@ -1,10 +1,14 @@
 // eslint-disable-next-line react/prop-types
+import Label from "./Label";
+
+// eslint-disable-next-line react/prop-types
 const Input = ({ id, type, name, placeholder }) => {
   return (
-    <div>
+    <div className="input-field">
+      <Label for="fullName" label="Full Name" />
       <input type={type} name={name} id={id} placeholder={placeholder} />
     </div>
   );
-}
+};
 
 export default Input;
---
+++ cv application/src/components/Label.jsx
@@ -0,0 +1,6 @@
+// eslint-disable-next-line react/prop-types
+const Label = ({ use, label }) => {
+  return <label htmlFor={use}>{label}</label>;
+};
+
+export default Label;
