3d2cd50e93770cd5ec76288f6e9f9cf70d00710f
Kummirai <ajaxmilton@hotmail.com>
2024-11-21 20:29:19 +0200

    Add work experience sectiion and style it

--- cv application/src/components/templateOne/Main.jsx
+++ cv application/src/components/templateOne/Main.jsx
@@ -1,11 +1,13 @@
 import AboutMe from "./AboutMe";
 import Heading from "./Heading";
+import WorkExperience from "./WorkExperience";
 
 const Main = () => {
   return (
     <div className="main">
       <Heading />
       <AboutMe />
+      <WorkExperience />
     </div>
   );
 };
