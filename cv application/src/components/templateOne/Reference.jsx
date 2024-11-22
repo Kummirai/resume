1271b8422b9f28d1b9aaf4f39434d8fde368c791
Kummirai <ajaxmilton@hotmail.com>
2024-11-22 12:00:49 +0200

    Add reference section and style it

---
+++ cv application/src/components/templateOne/Reference.jsx
@@ -0,0 +1,20 @@
+const Reference = () => {
+  return (
+    <div className="about-me">
+      <div>
+        <h2>Reference</h2>
+        <hr />
+        <p>Cole Baidoo</p>
+        <p>Director</p>
+        <p>+44 457 2564</p>
+      </div>
+      <div>
+        <p>Cole Baidoo</p>
+        <p>Director</p>
+        <p>+44 457 2564</p>
+      </div>
+    </div>
+  );
+};
+
+export default Reference;
--- cv application/src/components/templateOne/Sidebar.jsx
+++ cv application/src/components/templateOne/Sidebar.jsx
@@ -1,9 +1,11 @@
 import Education from "./Education";
+import Reference from "./Reference";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <Education />
+      <Reference />
     </div>
   );
 };
