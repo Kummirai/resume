31dcc934521ca44e4ea7d0f6a3944c5f327e90cb
Kummirai <ajaxmilton@hotmail.com>
2024-11-22 11:43:24 +0200

    Add Education section and style it

--- cv application/src/components/templateOne/Sidebar.jsx
+++ cv application/src/components/templateOne/Sidebar.jsx
@@ -1,5 +1,11 @@
+import Education from "./Education";
+
 const Sidebar = () => {
-  return <div className="sidebar">Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <Education />
+    </div>
+  );
 };
 
 export default Sidebar;
